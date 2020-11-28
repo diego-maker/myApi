0877faa4839ae9b699d8bca331e0549e89bfe542
diego-maker <62081532+diego-maker@users.noreply.github.com>
2020-11-28 20:59:51 -0300

    rotas translate

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
- 
-// my header
-var allowCrossDomain = function(req, res, next) {
+
+// my header    
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+
+    app.use('/produto', rotaProduto);
+    app.use('/', indexRota);
+    app.use('/usuario', rotaUsuario);
     next();
 }
 
@@ -21,12 +25,9 @@ const indexRota = require('./routes/index');
 const rotaProduto = require('./routes/produtos');
 const rotaUsuario = require('./routes/usuario');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-app.use('/produto', rotaProduto);
-app.use('/',indexRota);
-app.use('/usuario', rotaUsuario);
 
 module.exports = app
